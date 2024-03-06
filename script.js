09185b22c1d046acb22bd310422ede21f71da30a
explodeongo <arnipvan2004@gmail.com>
2024-03-06 16:11:03 +0530

    updated the script file a bit

--- script.js
+++ script.js
@@ -3,26 +3,22 @@ function getComputerChoice() {
   switch (num) {
     case 1:
       return "Rock";
+
     case 2:
       return "Paper";
-    case 3:
+
+    default:
       return "Scissors";
   }
 }
 
 let computerScore = 0;
 let playerScore = 0;
-const Rock = document.querySelector("#Rock");
-const Paper = document.querySelector("#Paper");
-const Scissors = document.querySelector("#Scissors");
-const Result = document.querySelector("#result");
-const computer = document.querySelector("#computer");
-const player = document.querySelector("#player");
 
-function PlayRound(playerselection, computerselection) {
-  switch (playerselection) {
+function playRound(playerSelection, computerSelection) {
+  switch (playerSelection) {
     case "rock":
-      switch (computerselection) {
+      switch (computerSelection) {
         case "Rock":
           return "The computer also played Rock. It is a tie";
         case "Paper":
@@ -33,61 +29,67 @@ function PlayRound(playerselection, computerselection) {
           return "The computer played Scissors. You win!";
       }
     case "paper":
-      switch (computerselection) {
-        case "Rock":
-          playerScore += 1;
-          return "The computer played Rock. You win!";
+      switch (computerSelection) {
         case "Paper":
           return "The computer also played Paper. It is a tie";
         case "Scissors":
           computerScore += 1;
-          return "The computer played Scissors. You Lose";
+          return "The computer played Scissors. You lose.";
+        case "Rock":
+          playerScore += 1;
+          return "The computer played Rock. You win!";
       }
-
     case "scissors":
-      switch (computerselection) {
+      switch (computerSelection) {
+        case "Scissors":
+          return "The computer also played Scissors. It is a tie";
         case "Rock":
           computerScore += 1;
-          return "The computer played Rock. You Lose";
+          return "The computer played Rock. You lose.";
         case "Paper":
           playerScore += 1;
           return "The computer played Paper. You win!";
-        case "Scissors":
-          return "The computer also played Scissors. It is a tie";
       }
   }
 }
 
+const Rock = document.querySelector("#Rock");
+const Paper = document.querySelector("#Paper");
+const Scissors = document.querySelector("#Scissors");
+
+const result = document.querySelector("#result");
+const computer = document.querySelector("#computer");
+const player = document.querySelector("#player");
 Rock.addEventListener("click", function () {
-  Result.textContent = PlayRound("rock", getComputerChoice());
+  result.textContent = playRound("rock", getComputerChoice());
   computer.textContent = "Computer Score:" + computerScore;
   player.textContent = "Player Score:" + playerScore;
   if (computerScore >= 5) {
-    Result.textContent = "The computer won. Better luck next time...";
+    result.textContent = "The computer won. Better luck next time...";
   }
   if (playerScore >= 5) {
-    Result.textContent = "You won!";
+    result.textContent = "You won!";
   }
 });
 Paper.addEventListener("click", function () {
-  Result.textContent = PlayRound("paper", getComputerChoice());
+  result.textContent = playRound("paper", getComputerChoice());
   computer.textContent = "Computer Score:" + computerScore;
   player.textContent = "Player Score:" + playerScore;
   if (computerScore >= 5) {
-    Result.textContent = "The computer won. Better luck next time...";
+    result.textContent = "The computer won. Better luck next time...";
   }
-  if (playerscore >= 5) {
-    Result.textContent = "You Won!";
+  if (playerScore >= 5) {
+    result.textContent = "You won!";
   }
 });
 Scissors.addEventListener("click", function () {
-  Result.textContent = PlayRound("scissors", getComputerChoice());
+  result.textContent = playRound("scissors", getComputerChoice());
   computer.textContent = "Computer Score:" + computerScore;
   player.textContent = "Player Score:" + playerScore;
   if (computerScore >= 5) {
-    Result.textContent = "The computer won. Better luck next time...";
+    result.textContent = "The computer won. Better luck next time...";
   }
   if (playerScore >= 5) {
-    Result.textContent = "You Won!";
+    result.textContent = "You won!";
   }
 });
